fcc370b1c6d0683c0bf481ddea386858bce63f87
Nazar Dakhno <dakhnonazar@gmail.com>
2021-09-10 13:20:08 +0300

    change show_stat method

--- spec/show_content_spec.rb
+++ spec/show_content_spec.rb
@@ -4,10 +4,14 @@ require_relative '../lib/user'
 
 RSpec.describe NewSuperCodebreaker2021::Game do
   let(:game) { described_class.new }
+  let(:file) { 'test.yml' }
+
+  after do
+    File.delete(file) if File.file?(file)
+  end
 
   context '#show_stats' do
-    let(:file) { 'test.yml' }
-    it 'should return sorted statistic by difficult, used attempts and used hints in table' do
+    it 'should return sorted statistic by difficult, used attempts and used hints' do
       user1 = User.new('User1', 0)
       user2 = User.new('User2', 0)
       user2.attempts_used += 5
@@ -15,8 +19,7 @@ RSpec.describe NewSuperCodebreaker2021::Game do
       game.save(user1, file)
       game.save(user2, file)
       game.save(user3, file)
-      expect(game.show_stats(file).is_a?(Terminal::Table)).to eq(true)
-      File.delete(file)
+      expect(game.show_stats(file).map(&:name)).to eq([user3.name, user1.name, user2.name])
     end
 
     it 'should return empty array if no any statistic' do
