4fdd96ff08db33c93d63187d5992cc14691782aa
Nazar Dakhno <dakhnonazar@gmail.com>
2021-09-14 12:47:45 +0300

    fix spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 require 'simplecov'
 require 'ffaker'
+require 'new_super_codebreaker_2021'
 
 SimpleCov.start do
   minimum_coverage 95
   add_filter 'spec'
   add_filter 'vendor'
 end
-
-require_relative '../lib/new_super_codebreaker_2021'
